259d46b7266695224b921edd03c1f8ea5311abfb
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2025-02-03 14:18:29 +0100

    add feedback form

--- frontend/src/views/FeedbackView.vue
+++ frontend/src/views/FeedbackView.vue
@@ -1,37 +1,39 @@
 <template>
     <div class="mx-auto m-4 w-11/12 md:w-1/2 lg:w-2/5">
         <h1 class="text-center text-2xl my-8">Give me some feedback!</h1>
-
-        <ValidatedForm
-            v-model="feedback"
-            :schema="schema"
-            @submit="submitFeedback"
-        >
-            <FormGroup label="Name">
-                <FormInput v-model="feedback.name" />
-            </FormGroup>
-
-            <FormGroup label="Feedback">
-                <FormTextArea v-model="feedback.text" />
-            </FormGroup>
-
-            <FormButton>Submit</FormButton>
-        </ValidatedForm>
+        <form @submit.prevent="handleSubmitClick" class="flex flex-col gap-4 px-4 pt-16 pb-32 bg-primary-400">
+            <div>
+                <label for="name" class="block">Name:</label>
+                <input id="name" class="w-full p-2" v-model="feedback.name" @keydown="showError.name = false"
+                    type="text" @blur="showError.name = true" />
+                <div class="text-red-500" v-if="showError.name && formattedError && formattedError.name">
+                    {{ formattedError.name._errors[0] }}
+                </div>
+            </div>
+            <div>
+                <label for="text" class="block">Feedback:</label>
+                <textarea id="text" class="w-full p-2" v-model="feedback.text" @keydown="showError.text = false"
+                    @blur="showError.text = true"></textarea>
+                <div class="text-red-500" v-if="showError.text && formattedError && formattedError.text">
+                    {{ formattedError.text._errors[0] }}
+                </div>
+            </div>
+            <button type="submit" class="border border-black p-4" :class="{
+                'bg-secondary-500 hover:bg-blue-700': formattedError === null,
+                'bg-gray-500 cursor-not-allowed': formattedError !== null,
+            }">
+                Send feedback
+            </button>
+        </form>
     </div>
 </template>
 
 <script setup lang="ts">
-import ValidatedForm from "@/components/form-elements/ValidatedForm.vue";
-import FormButton from "@/components/form-elements/FormButton.vue";
-import FormGroup from "@/components/form-elements/FormGroup.vue";
-import FormInput from "@/components/form-elements/FormInput.vue";
-import FormTextArea from "@/components/form-elements/FormTextArea.vue";
-
-import { reactive } from "vue";
-import { z } from "zod";
+import { reactive, computed, ref } from "vue";
+import { z, type ZodFormattedError } from "zod";
 
 const schema = z.object({
-    name: z.string().nonempty(),
+    name: z.string().nonempty("Name is required."),
     text: z.string().min(20),
 });
 
@@ -40,7 +42,27 @@ const feedback = reactive({
     text: "",
 });
 
-function submitFeedback() {
-    alert("Feedback submitted!");
+// Showing errors
+const showError = ref({
+    name: false,
+    text: false,
+});
+const formattedError = computed<ZodFormattedError<typeof feedback> | null>(() => {
+    const result = schema.safeParse(feedback);
+    return result.error?.format() ?? null;
+});
+
+// Submitting
+function handleSubmitClick() {
+    const result = schema.safeParse(feedback);
+
+    if (result.success) {
+        alert("Feedback submitted!" + JSON.stringify(result.data));
+    } else {
+        showError.value = {
+            name: true,
+            text: true,
+        };
+    }
 }
 </script>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,6 +7,7 @@ export default {
         extend: {
             colors: {
                 primary: colors.green,
+                secondary: colors.blue,
                 gray: colors.neutral,
             },
         },
