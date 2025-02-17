9c50ac091a8f557af7cc49f2e3e0d308f6be5589
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2025-02-17 23:59:44 +0100

    refac: move tailwind to plain CSS instead in FeedbackView component

--- frontend/src/views/FeedbackView.vue
+++ frontend/src/views/FeedbackView.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="mx-auto m-4 w-11/12 md:w-1/2 lg:w-2/5">
-        <h1 class="text-center text-2xl my-8">Give me some feedback!</h1>
-        <form @submit.prevent="handleSubmitClick" class="flex flex-col gap-4 px-4 pt-16 pb-32 bg-primary-400">
-            <div>
-                <label for="name" class="block">Name:</label>
-                <input id="name" class="w-full p-2" v-model="feedback.name" @keydown="showError.name = false"
-                    type="text" @blur="showError.name = true" />
-                <div class="text-red-500" v-if="showError.name && formattedError && formattedError.name">
-                    {{ formattedError.name._errors[0] }}
+    <div class="feedback-view">
+        <div class="form-container">
+            <h1 class="feedback-title">Give me some feedback!</h1>
+            <form @submit.prevent="handleSubmitClick">
+                <div>
+                    <label for="name">Name:</label>
+                    <input id="name" v-model="feedback.name" @keydown="showError.name = false" type="text"
+                        @blur="showError.name = true" />
+                    <div style="color: red;" v-if="showError.name && formattedError && formattedError.name">
+                        {{ formattedError.name._errors[0] }}
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label for="text" class="block">Feedback:</label>
-                <textarea id="text" class="w-full p-2" v-model="feedback.text" @keydown="showError.text = false"
-                    @blur="showError.text = true"></textarea>
-                <div class="text-red-500" v-if="showError.text && formattedError && formattedError.text">
-                    {{ formattedError.text._errors[0] }}
+                <div>
+                    <label for="text">Feedback:</label>
+                    <textarea id="text" v-model="feedback.text" @keydown="showError.text = false"
+                        @blur="showError.text = true"></textarea>
+                    <div style="color: red;" v-if="showError.text && formattedError && formattedError.text">
+                        {{ formattedError.text._errors[0] }}
+                    </div>
                 </div>
-            </div>
-            <button type="submit" class="border border-black p-4" :class="{
-                'bg-secondary-500 hover:bg-blue-700': formattedError === null,
-                'bg-gray-500 cursor-not-allowed': formattedError !== null,
-            }">
-                Send feedback
-            </button>
-        </form>
+                <!-- This double bang trick (!!) uses JavaScript's type coercion to turn any variable to a boolean value -->
+                <button type="submit" :disabled="!!formattedError">
+                    Send feedback
+                </button>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -47,6 +47,7 @@ const showError = ref({
     name: false,
     text: false,
 });
+
 const formattedError = computed<ZodFormattedError<typeof feedback> | null>(() => {
     const result = schema.safeParse(feedback);
     return result.error?.format() ?? null;
@@ -66,3 +67,72 @@ function handleSubmitClick() {
     }
 }
 </script>
+
+<style scoped>
+.feedback-view {
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+}
+
+.form-container {
+    grid-column: 2 / -2;
+}
+
+@media (min-width: 768px) {
+    .form-container {
+        grid-column: 3 / -3;
+    }
+}
+
+@media (min-width: 1024px) {
+    .form-container {
+        grid-column: 4 / -4;
+    }
+}
+
+@media (min-width: 1280px) {
+    .form-container {
+        grid-column: 5 / -5;
+    }
+}
+
+.feedback-title {
+    text-align: center;
+    font-size: var(--text-2xl);
+    margin-top: var(--spacing-8);
+    margin-bottom: var(--spacing-8);
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-4);
+    padding: var(--spacing-16) var(--spacing-4) var(--spacing-32) var(--spacing-4);
+
+    background-color: var(--color-primary-400);
+}
+
+input#name,
+textarea#text {
+    width: 100%;
+    padding: var(--spacing-2);
+}
+
+button {
+    border: 1px solid black;
+    padding: var(--spacing-4);
+}
+
+button:disabled {
+    background-color: var(--color-neutral-500);
+    cursor: not-allowed;
+}
+
+button:not(:disabled) {
+    background-color: var(--color-primary-400);
+}
+
+button:not(:disabled):hover {
+    background-color: var(--color-primary-500);
+}
+</style>
