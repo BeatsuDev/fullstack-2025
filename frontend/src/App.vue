9e0f4625814be7f88ad368752958240c6d64e350
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2025-02-17 23:37:51 +0100

    refac: move tailwind to plain CSS instead in App component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,7 @@
 <template>
-    <nav
-        class="w-full bg-gray-700 p-4 text-white text-3xl flex justify-center gap-8"
-    >
-        <RouterLink
-            v-for="(route, index) in navigationRoutes"
-            :key="index"
-            :to="route.path"
-            class="duration-150"
-            active-class="text-primary-500"
-        >
+    <nav>
+        <RouterLink v-for="(route, index) in navigationRoutes" :key="index" :to="route.path" class="link"
+            active-class="active-link">
             {{ route.text }}
         </RouterLink>
     </nav>
@@ -23,3 +16,27 @@ const navigationRoutes = [
     { path: "/feedback", text: "Feedback" },
 ] as const;
 </script>
+
+<style scoped>
+nav {
+    display: flex;
+    justify-content: center;
+    gap: var(--spacing-8);
+
+    font-size: var(--text-3xl);
+    color: var(--color-neutral-50);
+    background-color: var(--color-neutral-700);
+
+    padding: var(--spacing-4);
+    width: 100%;
+}
+
+.link {
+    transition: color 150ms;
+}
+
+/* The link at which the router is currently at */
+.active-link {
+    color: var(--color-primary-500);
+}
+</style>
