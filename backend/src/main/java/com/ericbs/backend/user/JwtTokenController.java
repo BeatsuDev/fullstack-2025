e266540a12717f418d1469e9cf07665a35508ee9
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2025-03-07 17:06:24 +0100

    refac: renamed authentication controller to jwt token controller

--- backend/src/main/java/com/ericbs/backend/user/JwtTokenController.java
+++ backend/src/main/java/com/ericbs/backend/user/JwtTokenController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class AuthenticationController {
+public class JwtTokenController {
     @PostMapping("/user/token")
     public ResponseEntity<Object> createSession(@RequestBody LoginRequestDTO loginRequest) {
         return ResponseEntity.status(501).body(new GenericErrorDTO("Not implemented yet."));
