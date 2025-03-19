e5c8af519f5fa5b4311f14d6382203f54f828f31
BeatsuDev <ericbieszczad@gmail.com>
2025-03-19 03:18:22 +0100

    fix: route not always changing

--- frontend/src/router/middleware/layout.ts
+++ frontend/src/router/middleware/layout.ts
@@ -6,12 +6,17 @@ export async function layoutMiddleware(
     from: Parameters<NavigationGuard>[1],
     next: Parameters<NavigationGuard>[2]
 ) {
+    if (!to.meta.layout) {
+        to.meta.layoutComponent = DefaultLayout;
+        return next();
+    }
+    
     try {
-        to.meta.layoutComponent = await import(
-            `@/layouts/${to.meta.layout}.vue`
-        );
+        import(`@/layouts/${to.meta.layout}.vue`)
+            .then(() => to.meta.layoutComponent = DefaultLayout)
+            .then(next);
     } catch (e) {
         to.meta.layoutComponent = DefaultLayout;
+        next();
     }
-    next();
 }
